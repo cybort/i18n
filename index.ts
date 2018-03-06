47636ca882b028e77ca5f4ddf5af7e22191508dc
trevj <trevj>
2018-03-06 14:52:07 -0500

    format code

--- index.ts
+++ index.ts
@@ -15,9 +15,11 @@
 interface LanguageObj {
   id: string;
 }
-type LanguageMap = {[id: string]: LanguageObj};
-type Languages = string[] | LanguageObj[] | LanguageMap;
-type LanguageLookup = (languageId: string) => string | undefined;
+type LanguageMap = {
+  [id: string]: LanguageObj
+};
+type Languages = string[]|LanguageObj[]|LanguageMap;
+type LanguageLookup = (languageId: string) => string|undefined;
 
 const toLowerCase = String.prototype.toLowerCase.call.bind(String.prototype.toLowerCase);
 
@@ -25,15 +27,16 @@ const nav = navigator;
 const browserLanguages = (nav.languages || [nav.language]) as string[];
 
 function makeLookUpLanguage(availableLanguages: Languages, normalize = toLowerCase) {
-  return (languageId: string): string | undefined => {
+  return (languageId: string): string|undefined => {
     languageId = normalize(languageId);
     const isNonEmptyArray = Array.isArray(availableLanguages) && availableLanguages.length > 0;
-    const isFirstElementString = isNonEmptyArray && (
-      availableLanguages as string[] | LanguageObj[])[0] instanceof String;
-    const availableLanguageIds: string[] = isNonEmptyArray ? (
-        isFirstElementString ? (availableLanguages as string[]) :
-        (availableLanguages as LanguageObj[]).map((langObj) => langObj.id)
-    ) : Object.keys(availableLanguages as LanguageMap);
+    const isFirstElementString =
+        isNonEmptyArray && (availableLanguages as string[] | LanguageObj[])[0] instanceof String;
+    const availableLanguageIds: string[] = isNonEmptyArray ?
+        (isFirstElementString ?
+             (availableLanguages as string[]) :
+             (availableLanguages as LanguageObj[]).map((langObj) => langObj.id)) :
+        Object.keys(availableLanguages as LanguageMap);
     for (const availableLanguageId of availableLanguageIds) {
       const parts = normalize(availableLanguageId).split('-');
       while (parts.length) {
@@ -49,12 +52,11 @@ function makeLookUpLanguage(availableLanguages: Languages, normalize = toLowerCa
 
 // tslint:disable-next-line:no-any
 (window as any).OutlineI18n = {
-  getBestMatchingLanguage(
-    available: LanguageLookup | Languages,
-    preferred = browserLanguages
-  ): string | undefined {
-    const lookUpAvailable = typeof available === 'function' ?
-        available : makeLookUpLanguage(available);
+  getBestMatchingLanguage(available: LanguageLookup|Languages, preferred = browserLanguages):
+      string |
+  undefined {
+    const lookUpAvailable =
+        typeof available === 'function' ? available : makeLookUpLanguage(available);
     for (const candidate of preferred) {
       const parts = candidate.split('-');
       while (parts.length) {
